ce50f28f6b6682a759ca900cfc00da7dd203cb54
kinitec33 <kininla7@gmail.com>
2026-08-20 23:19:28 +0100

    Build services catalogue foundation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Layout from './components/Layout'
 import Home from './pages/Home'
+import Services from './pages/Services'
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
         </Route>
       </Routes>
     </BrowserRouter>
